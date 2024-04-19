1aebe7d3ff49dff2d27999cce805e72f722239a8
BROWb <127416062+BROWb@users.noreply.github.com>
2024-04-19 11:08:06 -0300

    v2o

---
+++ componentes.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>componentes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="container">
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php">HOME</a>
+                </li>
+                <li><a href="componentes.php">PERIFÉRICOS</a>
+                </li>
+                <li><a href="quemsomos.php">QUEM SOMOS</a>
+                </li>
+                <li><a href="contatos.php">CONTATOS</a>
+                </li>
+            </ul>
+
+            <form>
+                <input type="text">
+                <button>pesquisa</button>
+            </form>
+
+        </nav>
+          
+        <div>
+            <h1>O que é placa-mãe</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <article>
+           <br>
+           <h2>A placa-mãe é a base do computador. Ela contém a CPU e é uma espécie de central por onde todos os outros componentes de hardware passam. A placa-mãe age como o cérebro do computador, alocando potência onde necessário e comunicando-se com todos os componentes e coordenando-os, o que a torna uma das peças de hardware mais importantes de um computador.
+            Ao escolher uma placa-mãe, é importante checar que portas de hardware ela oferece. É vital ver o nível das portas USB oferecidas (USB 2.0, 3.0, 3.1), além dos tipos de porta de tela usados (HDMI, DVI, RGB) e a quantidade total de cada uma. As portas da placa-mãe também ajudam a entender quais outros componentes de hardware serão compatíveis com seu computador, como o tipo de RAM e de placa de vídeo que você pode usar.
+            Embora a placa-mãe seja só uma peça do circuito inteiro, é nela que uma das peças de hardware mais importantes: o processador.
+           </h2>
+        
+        </article>
+        <img src="placam.png" id="img1">
+    </section>
+</body>
+</html>
--- contatos.php
+++ contatos.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contatos</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -25,20 +25,8 @@
                 <input type="text">
                 <button>pesquisa</button>
             </form>
+
         </nav>
-
-        <div class="img2">
-        <img src="img1.png">
-        </div>
-
-        <article>
-           <h3>Quer saber qual hardware o seu computador possui? Torne-se um especialista em informática com nosso guia rápido sobre esses componentes essenciais e suas funções.
-           </h3>
-           <br>
-           <h4>Em uma definição bem simples, o termo hardware refere-se aos componentes físicos de que um computador precisa para funcionar. O termo envolve tudo que tenha uma placa de circuito e opere dentro de um computador ou notebook, como placa-mãe, placa de vídeo, CPU (unidade de processamento central), ventoinhas, webcam, fonte de alimentação e assim por diante.
-           </h4>
-        </article>
     </section>
-
 </body>
 </html>
