524b64d677ce548ab2938f2f0462e8b7773aec47
BROWb <127416062+BROWb@users.noreply.github.com>
2024-04-23 11:08:20 -0300

    v3
    add de img

--- componentes.php
+++ componentes.php
@@ -27,20 +27,165 @@
             </form>
 
         </nav>
-          
+    
         <div>
             <h1>O que é placa-mãe</h1>
             <hr size="6px" width="100%" color="#3b0063"></hr>
         </div>
-        <article>
-           <br>
-           <h2>A placa-mãe é a base do computador. Ela contém a CPU e é uma espécie de central por onde todos os outros componentes de hardware passam. A placa-mãe age como o cérebro do computador, alocando potência onde necessário e comunicando-se com todos os componentes e coordenando-os, o que a torna uma das peças de hardware mais importantes de um computador.
-            Ao escolher uma placa-mãe, é importante checar que portas de hardware ela oferece. É vital ver o nível das portas USB oferecidas (USB 2.0, 3.0, 3.1), além dos tipos de porta de tela usados (HDMI, DVI, RGB) e a quantidade total de cada uma. As portas da placa-mãe também ajudam a entender quais outros componentes de hardware serão compatíveis com seu computador, como o tipo de RAM e de placa de vídeo que você pode usar.
-            Embora a placa-mãe seja só uma peça do circuito inteiro, é nela que uma das peças de hardware mais importantes: o processador.
-           </h2>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2>A placa-mãe é a base do computador. Ela contém a CPU e é uma espécie de central por onde todos os outros componentes de hardware passam. A placa-mãe age como o cérebro do computador, alocando potência onde necessário e comunicando-se com todos os componentes e coordenando-os, o que a torna uma das peças de hardware mais importantes de um computador.
+                 Ao escolher uma placa-mãe, é importante checar que portas de hardware ela oferece. É vital ver o nível das portas USB oferecidas (USB 2.0, 3.0, 3.1), além dos tipos de porta de tela usados (HDMI, DVI, RGB) e a quantidade total de cada uma. As portas da placa-mãe também ajudam a entender quais outros componentes de hardware serão compatíveis com seu computador, como o tipo de RAM e de placa de vídeo que você pode usar.
+                Embora a placa-mãe seja só uma peça do circuito inteiro, é nela que uma das peças de hardware mais importantes: o processador.
+                </h2>
+                </article>
+                </td>
+                <td>
+                <div class="img1">
+                <img src="placam.png">
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+            <h1>O que é uma CPU (processador/unidade de processamento central)</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2>CPU é a sigla para Central Process Unit, ou Unidade Central de Processamento. Ele é o principal item de Hardware do computador, que também é conhecido como processador. A CPU é responsável por calcular e realizar tarefas determinadas pelo usuário e é considerado o cérebro do PC.
+Uma CPU é feita de bilhões de transistores, que são pequenas peças microscópicas que controlam a corrente elétrica do componente.
+                </h2>
+                </article>
+                </td>
+                <td>
+                <div class="img3">
+                <img src="cpu.png" >
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+        <div>
+            <h1>O que é memoria RAM?</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <article class="ram">
+                <br>
+                <h2>A memória de acesso aleatório, ou RAM, é o hardware encontrado nas entradas de memória da placa-mãe. O papel da RAM é armazenar em caráter instantâneo e temporário as informações criadas por programas e fazer isso de forma que os dados possam ser acessados depois imediatamente. Algumas das tarefas que exigem memória aleatória, ou RAM, são: renderizar imagens para design gráfico, vídeos ou fotografias editadas, trabalhar em diversos aplicativos ao mesmo tempo (por exemplo, ter um jogo aberto em uma tela e conversar no Discord em outra).
+                </h2>
+                </article>
+              
+              
+                <div class="ram">
+                <img src="ram.png" >
+
+                </div>
+            
+                
+            
         
-        </article>
-        <img src="placam.png" id="img1">
+
+
+        <div>
+            <h1>O que é um disco rígido?</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2>O disco rígido é o dispositivo de armazenamento responsável por armazenar dados permanentes e temporários. Esses dados vêm de muitas formas diferentes, mas, a grosso modo, tudo é salvo ou instalado no computador: por exemplo, programas de computador, fotos da família, o sistema operacional, documentos de processamento de texto e muitos outros.
+
+                </article>
+                </td>
+                <td>
+                <div class="img5">
+                <img src="placam.png" >
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+            <h1>O que é um ssd?</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2> é um componente que armazena dados em computadores e consoles de videogame. Ele substitui o HDD (Hard Disk Drive) tradicional e ainda garante mais rapidez ao sistema, graças às velocidades maiores de leitura e gravação.
+                </article>
+                </td>
+                <td>
+                <div class="img6">
+                <img src="placam.png" >
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+            <h1>O que é uma unidade de processamento gráfico (GPU)?</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2> Importante principalmente para renderizações 3D, a GPU faz exatamente o que seu nome sugere: processa blocos enormes de dados gráficos. Você verá que a placa de vídeo do seu computador tem pelo menos uma GPU. Diferentemente dos recursos gráficos integrados básicos que as placas-mãe oferecem, as placas de vídeo dedicadas se conectam à placa-mãe por meio de uma entrada de expansão para trabalhar quase que exclusivamente na renderização gráfica. Isso também significa que você pode trocar sua placa de vídeo se quiser melhorar um pouco o desempenho do seu computador.
+E não é só isso: as GPUs modernas cumprem um papel computacional mais amplo que não se limita a renderizar, o que as torna uma extensão da unidade de processamento central, ou CPU.
+
+                </article>
+                </td>
+                <td>
+                <div class="img7">
+                <img src="placam.png" >
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+        <div>
+            <h1>O que é uma unidade de alimentação (PSU)?</h1>
+            <hr size="6px" width="100%" color="#3b0063"></hr>
+        </div>
+        <table>
+            <tr>
+                <td><article>
+                <br>
+                <h2> Uma unidade de alimentação, comumente abreviada como PSU, faz mais do que só fornecer energia para o computador. A energia de uma fonte de energia externa entra no sistema por meio dela e é alocada pela placa-mãe a componentes de hardware. Porém, nem todas as unidades, ou fontes de alimentação, são iguais, e sem uma PSU com a tensão certa, seu sistema não funciona.
+Geralmente, um computador moderno precisa de uma PSU com classificação entre 500 W e 850 W para efetivamente alimentar todos os componentes, embora o tamanho dela dependa exclusivamente do consumo de energia do sistema. Computadores usados para tarefas de alta intensidade, como o design gráfico ou jogos, exigem componentes mais potentes que, por consequência, precisarão de uma PSU maior para suprir essa necessidade adicional de energia.
+Sem a quantidade certa de energia, os componentes não funcionarão como deveriam e o computador pode passar por falhas ou simplesmente nem ligar. É recomendado ter uma fonte de alimentação que cubra mais do que o uso do seu sistema. Isso não só protege contra falhas do sistema, mas também dispensa uma eventual troca se você decida trocar seus componentes por outros mais potentes no futuro.
+Conhecer seu computador e os componentes dele pode ser muito útil na hora de modernizar, trocar peças, ou até mesmo montar um computador. Caso surja um problema interno no computador, você terá um maior conhecimento da importância de cada componente, da necessidade de estarem em boas condições de funcionamento e saberá como agir para resolver problemas. 
+                </article>
+                </td>
+                <td>
+                <div class="img8">
+                <img src="placam.png" >
+
+                </div>
+            
+                </td>
+            </tr>
+        </table>
+
+
     </section>
 </body>
 </html>
